850e88513bcb6d0dbe00e968f17d45c8f4d3b56c
RiyaRani23 <riyarani1404@gmail.com>
2026-01-22 14:29:17 +0530

    Adding Course page

--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+import Course from "./Course";
+
+const courses = [1,2,3,4,5,6];
 
 const Courses = () => {
-    const isLoading = true;
+  const isLoading = false;
   return (
     <div className="bg-gray-50">
-        <div className='max-w-7xl mx-auto p-6'>
-            <h2 className='font-bold text-3xl text-center mb-10 '>Our Courses</h2>
-            {isLoading ? (
-                <p>Loading courses...</p>
-            ) : (<p>No courses available.</p>)}
+      <div className="max-w-7xl mx-auto p-6">
+        <h2 className="font-bold text-3xl text-center mb-10 ">Our Courses</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {isLoading ? (
+            Array.from({ length: 6 }).map((_, index) => (
+              <CourseSkeleton key={index} />
+            ))
+          ) : (
+            courses.map((course, index) => <Course key={index}/>)
+          )}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
+
+//Shimmer for Course
+
+const CourseSkeleton = () => {
+  return (
+    <div
+      className="max-w-sm rounded-2xl border border-slate-200 dark:border-slate-800 
+      bg-white dark:bg-slate-900 p-4 shadow-sm"
+    >
+      {/* Course Thumbnail */}
+      <Skeleton className="h-40 w-full rounded-xl" />
+
+      {/* Title */}
+      <div className="mt-4 space-y-2">
+        <Skeleton className="h-4 w-3/4" />
+        <Skeleton className="h-4 w-1/2" />
+      </div>
+
+      {/* Instructor */}
+      <div className="flex items-center gap-3 mt-4">
+        <Skeleton className="h-10 w-10 rounded-full" />
+        <Skeleton className="h-4 w-1/3" />
+      </div>
+
+      {/* CTA Button */}
+      <Skeleton className="h-10 w-full rounded-full mt-5" />
+    </div>
+  );
+};
